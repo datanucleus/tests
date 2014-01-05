9d4a73dd03c183eec16fe534a865377f790a60ad
andyjefferson <andy@datanucleus.org>
2014-01-05 08:34:46 +0000

    Add test for NULLS FIRST|LAST

--- jdo/rdbms/src/test/org/datanucleus/tests/JDOQLBasicTest.java
+++ jdo/rdbms/src/test/org/datanucleus/tests/JDOQLBasicTest.java
@@ -20,6 +20,7 @@ package org.datanucleus.tests;
 import java.util.Calendar;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
 
 import javax.jdo.PersistenceManager;
 import javax.jdo.Query;
@@ -121,6 +122,78 @@ public class JDOQLBasicTest extends JDOPersistenceTestCase
             
             c = (Collection) q.execute();
             assertEquals(3, c.size());
+
+            tx.commit();
+
+        }
+        finally
+        {
+            if (tx.isActive())
+            {
+                tx.rollback();
+            }
+            pm.close();
+
+            // Clean out our data
+            clean(Office.class);
+        }
+    }
+
+    /**
+     * Tests the extension for location of NULLS within an ORDER BY clause.
+     */
+    public void testOrderNullsFirstLast()
+    {
+        RDBMSStoreManager srm = (RDBMSStoreManager)storeMgr;
+        if (!srm.getDatastoreAdapter().supportsOption(DatastoreAdapter.ORDERBY_NULLS_DIRECTIVES))
+        {
+            // Not supported so it passed :-)
+            return;
+        }
+
+        PersistenceManager pm = pmf.getPersistenceManager();
+        Transaction tx = pm.currentTransaction();
+        try
+        {
+            // Persist some simple objects (uses datastore id, or composite application id depending on suite)
+            tx.begin();
+
+            for (int i=0;i<3;i++)
+            {
+                Office off = new Office(i, "Colour" + i, null);
+                pm.makePersistent(off);
+            }
+            Office o1 = new Office(3, "Green", "Big spacious office");
+            Office o2 = new Office(4, "Blue", "Pokey office at the back of the building");
+            Office o3 = new Office(5, "Yellow", "Massive open plan office");
+            pm.makePersistent(o1);
+            pm.makePersistent(o2);
+            pm.makePersistent(o3);
+            tx.commit();
+
+            tx.begin();
+
+            Query q = pm.newQuery("SELECT FROM " + Office.class.getName() + " ORDER BY description NULLS FIRST");
+            List<Office> offices = (List<Office>) q.execute();
+            assertEquals(6, offices.size());
+            Iterator<Office> it = offices.iterator();
+            assertNull("First result (NULLS FIRST) should have null description", it.next().getDescription());
+            assertNull("Second result (NULLS FIRST) should have null description", it.next().getDescription());
+            assertNull("Third result (NULLS FIRST) should have null description", it.next().getDescription());
+            assertNotNull("Fourth result (NULLS FIRST) should have not null description", it.next().getDescription());
+            assertNotNull("Fifth result (NULLS FIRST) should have not null description", it.next().getDescription());
+            assertNotNull("Sixth result (NULLS FIRST) should have not null description", it.next().getDescription());
+
+            q = pm.newQuery("SELECT FROM " + Office.class.getName() + " ORDER BY description NULLS LAST");
+            offices = (List<Office>) q.execute();
+            assertEquals(6, offices.size());
+            it = offices.iterator();
+            assertNotNull("First result (NULLS FIRST) should have not null description", it.next().getDescription());
+            assertNotNull("Second result (NULLS FIRST) should have not null description", it.next().getDescription());
+            assertNotNull("Third result (NULLS FIRST) should have not null description", it.next().getDescription());
+            assertNull("Fourth result (NULLS FIRST) should have null description", it.next().getDescription());
+            assertNull("Fifth result (NULLS FIRST) should have null description", it.next().getDescription());
+            assertNull("Sixth result (NULLS FIRST) should have null description", it.next().getDescription());
 
             tx.commit();
 
