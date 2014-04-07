ee85ec0f2edb9b04a793c9f69a6bc705fd28d1a3
andyjefferson <andy@datanucleus.org>
2014-04-07 14:08:17 +0100

    Add copyright headers, and include class name in test "name"

--- framework/src/java/org/datanucleus/tests/DatanucleusTestWatcher.java
+++ framework/src/java/org/datanucleus/tests/DatanucleusTestWatcher.java
@@ -1,3 +1,20 @@
+/******************************************************************
+Copyright (c) 2014 Renato Garcia and others. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Contributors:
+    ...
+*****************************************************************/
 package org.datanucleus.tests;
 
 import java.util.stream.Stream;
@@ -12,20 +29,19 @@ import org.junit.runner.Description;
  */
 public class DatanucleusTestWatcher extends TestWatcher
 {
-
     private Description description;
-    
-    public String getTestName() {
-        return description.getMethodName();
+
+    public String getTestName() 
+    {
+        return description.getTestClass().getName() + " " + description.getMethodName();
     }
 
     @Override
     protected void starting(Description description)
     {
         this.description = description;
-        
-        Class<?> testClass = description.getTestClass();
 
+        Class<?> testClass = description.getTestClass();
         if (JDOPersistenceTestCase.class.isAssignableFrom(testClass))
         {
             filterDatastores(testClass);
@@ -35,27 +51,23 @@ public class DatanucleusTestWatcher extends TestWatcher
     private void filterDatastores(Class<?> testClass)
     {
         // Use class level annotation as default if present
-        final DatastoreKey[] defaultDatastores = testClass.isAnnotationPresent(Datastore.class)
-                ? testClass.getAnnotation(Datastore.class).value()
-                : null;
+        final DatastoreKey[] defaultDatastores = testClass.isAnnotationPresent(Datastore.class) ?
+                testClass.getAnnotation(Datastore.class).value() : null;
 
         // Use method level annotation if present otherwise fallback to the default.
-        DatastoreKey[] filterDatastores = description.getAnnotation(Datastore.class) == null
-                ? defaultDatastores
-                : description.getAnnotation(Datastore.class).value();
+        DatastoreKey[] filterDatastores = description.getAnnotation(Datastore.class) == null ?
+                defaultDatastores : description.getAnnotation(Datastore.class).value();
 
         // Any datastore to filter?
         if (filterDatastores != null)
         {
             String datastoreKey = JDOPersistenceTestCase.storeMgr.getStoreManagerKey();
             DatastoreKey currentDatastore = DatastoreKey.valueOf(datastoreKey);
-
-            DatastoreKey vendorIdDatastore = JDOPersistenceTestCase.vendorID == null
-                    ? null : DatastoreKey.valueOf(JDOPersistenceTestCase.vendorID);
+            DatastoreKey vendorIdDatastore = JDOPersistenceTestCase.vendorID == null ?
+                    null : DatastoreKey.valueOf(JDOPersistenceTestCase.vendorID);
 
             Assume.assumeTrue(
-                    Stream.of(filterDatastores)
-                            .anyMatch(datastore -> datastore.equals(currentDatastore) || datastore.equals(vendorIdDatastore)));
+                    Stream.of(filterDatastores).anyMatch(datastore -> datastore.equals(currentDatastore) || datastore.equals(vendorIdDatastore)));
         }
     }
 }
