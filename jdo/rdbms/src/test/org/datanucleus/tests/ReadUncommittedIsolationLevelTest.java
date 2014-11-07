cc82501700c980ecd1e92b903320e942ce82d3e8
andyjefferson <andy@datanucleus.org>
2014-11-07 13:56:19 +0000

    Add check on whether the datastore supports this isolation level, since tests are supposed to run with all RDBMS. Reformat it to look like other tests

--- jdo/rdbms/src/test/org/datanucleus/tests/ReadUncommittedIsolationLevelTest.java
+++ jdo/rdbms/src/test/org/datanucleus/tests/ReadUncommittedIsolationLevelTest.java
@@ -29,22 +29,6 @@ public class ReadUncommittedIsolationLevelTest extends JDOPersistenceTestCase
 {
     private static boolean initialised = false;
 
-    private Object oid;
-
-    private PersistenceManager pm1;
-
-    private PersistenceManager pm2;
-
-    private Transaction tx;
-
-    private Transaction tx1;
-
-    private Transaction tx2;
-
-    private Office o1;
-
-    private Office o2;
-
     private static final String ROOM = "room";
 
     public ReadUncommittedIsolationLevelTest(String name)
@@ -59,56 +43,19 @@ public class ReadUncommittedIsolationLevelTest extends JDOPersistenceTestCase
 
     public void testReadUncommited()
     {
-        _init();
-        String finalDescription = null;
+        if (!pmf.supportedOptions().contains("javax.jdo.option.TransactionIsolationLevel.read-uncommitted"))
+        {
+            // Datastore doesn't support this isolation level
+            return;
+        }
+
+        // Add some initial data
+        pm = pmf.getPersistenceManager();
+        pm.setProperty(PropertyNames.PROPERTY_CACHE_L2_TYPE, "none");
+        Transaction tx = pm.currentTransaction();
+        Object oid;
         try
         {
-            pm1 = pmf.getPersistenceManager();
-            pm1.setProperty(PropertyNames.PROPERTY_CACHE_L2_TYPE, "none");
-            tx1 = pm1.currentTransaction();
-            tx1.setIsolationLevel(Constants.TX_READ_UNCOMMITTED);
-            tx1.begin();
-            pm2 = pmf.getPersistenceManager();
-            pm2.setProperty(PropertyNames.PROPERTY_CACHE_L2_TYPE, "none");
-            tx2 = pm2.currentTransaction();
-            tx2.setIsolationLevel(Constants.TX_READ_UNCOMMITTED);
-            tx2.begin();
-            o1 = (Office) pm1.getObjectById(oid);
-
-            LOG.info("within tx1 after modifying:" + o1.asString());
-            finalDescription = o1.getDescription() + o1.getRoomName();
-            o1.setDescription(finalDescription);
-            // send UPDATE to database
-            pm1.flush();
-
-            LOG.info("within tx1 after modifying:" + o1.asString());
-            o2 = (Office) pm2.getObjectById(oid);
-            LOG.info("within tx2: " + o2.asString());
-            assertEquals("uncommited modification not seen", finalDescription, o2.getDescription());
-        }
-        catch (JDODataStoreException e)
-        {
-            assertFalse("Should be able to see description " + finalDescription + " but " + e.getMessage(), true);
-        }
-        finally
-        {
-            tx2.commit();
-            pm2.close();
-            tx1.commit();
-            pm1.close();
-            clean(Office.class);
-            clean(Department.class);
-            closePMF();
-        }
-    }
-
-    private void _init()
-    {
-        try
-        {
-            pm = pmf.getPersistenceManager();
-            pm.setProperty(PropertyNames.PROPERTY_CACHE_L2_TYPE, "none");
-            tx = pm.currentTransaction();
             tx.begin();
             // create sample data
             Office o = new Office(1L, ROOM, "desc");
@@ -124,7 +71,48 @@ public class ReadUncommittedIsolationLevelTest extends JDOPersistenceTestCase
             }
             pm.close();
         }
-        o1 = null;
-        o2 = null;
+
+        String finalDescription = null;
+        PersistenceManager pm1 = pmf.getPersistenceManager();
+        PersistenceManager pm2 = pmf.getPersistenceManager();
+        Transaction tx1 = pm1.currentTransaction();
+        Transaction tx2 = pm2.currentTransaction();
+        try
+        {
+            pm1.setProperty(PropertyNames.PROPERTY_CACHE_L2_TYPE, "none");
+            tx1.setIsolationLevel(Constants.TX_READ_UNCOMMITTED);
+            tx1.begin();
+
+            pm2.setProperty(PropertyNames.PROPERTY_CACHE_L2_TYPE, "none");
+            tx2.setIsolationLevel(Constants.TX_READ_UNCOMMITTED);
+            tx2.begin();
+
+            Office o1 = (Office) pm1.getObjectById(oid);
+
+            LOG.info("within tx1 after modifying:" + o1.asString());
+            finalDescription = o1.getDescription() + o1.getRoomName();
+            o1.setDescription(finalDescription);
+            // send UPDATE to database
+            pm1.flush();
+
+            LOG.info("within tx1 after modifying:" + o1.asString());
+            Office o2 = (Office) pm2.getObjectById(oid);
+            LOG.info("within tx2: " + o2.asString());
+            assertEquals("uncommited modification not seen", finalDescription, o2.getDescription());
+        }
+        catch (JDODataStoreException e)
+        {
+            assertFalse("Should be able to see description " + finalDescription + " but " + e.getMessage(), true);
+        }
+        finally
+        {
+            tx2.commit();
+            pm2.close();
+            tx1.commit();
+            pm1.close();
+
+            clean(Office.class);
+            clean(Department.class);
+        }
     }
 }
