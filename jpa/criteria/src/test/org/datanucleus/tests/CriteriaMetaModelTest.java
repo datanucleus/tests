8a629b029d04d4b57974aac1dce8641515e90e5e
andyjefferson <andy@datanucleus.org>
2015-05-13 18:15:47 +0100

    test for NUCJPA-307

--- jpa/criteria/src/test/org/datanucleus/tests/CriteriaMetaModelTest.java
+++ jpa/criteria/src/test/org/datanucleus/tests/CriteriaMetaModelTest.java
@@ -272,6 +272,61 @@ public class CriteriaMetaModelTest extends JPAPersistenceTestCase
     /**
      * Test basic generation of query with candidate and alias.
      */
+    public void testCandidateWithoutSelect()
+    {
+        EntityManager em = getEM();
+        EntityTransaction tx = em.getTransaction();
+        try
+        {
+            tx.begin();
+
+            CriteriaBuilder qb = emf.getCriteriaBuilder();
+            CriteriaQuery<Team> crit = qb.createQuery(Team.class);
+            Root<Team> candidate = crit.from(Team.class);
+            candidate.alias("t");
+
+            // DN extension
+            assertEquals("Generated JPQL query is incorrect",
+                "SELECT t FROM org.datanucleus.samples.jpa.query.Team t", crit.toString());
+
+            Query q = em.createQuery(crit);
+            List<Team> teams = q.getResultList();
+
+            assertNotNull("Null results returned!", teams);
+            assertEquals("Number of results is incorrect", 2, teams.size());
+            boolean realmadrid = false;
+            boolean barcelona = false;
+            Iterator<Team> teamIter = teams.iterator();
+            while (teamIter.hasNext())
+            {
+                Team team = teamIter.next();
+                if (team.getName().equals("Barcelona"))
+                {
+                    barcelona = true;
+                }
+                else if (team.getName().equals("Real Madrid"))
+                {
+                    realmadrid = true;
+                }
+            }
+            assertTrue("Barcelona not returned", barcelona);
+            assertTrue("Real Madrid not returned", realmadrid);
+
+            tx.rollback();
+        }
+        finally
+        {
+            if (tx.isActive())
+            {
+                tx.rollback();
+            }
+            em.close();
+        }
+    }
+
+    /**
+     * Test basic generation of query with candidate and alias.
+     */
     public void testCandidateDistinct()
     {
         EntityManager em = getEM();
