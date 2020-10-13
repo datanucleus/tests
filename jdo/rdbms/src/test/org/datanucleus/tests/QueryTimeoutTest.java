40dd895dd96814a0b8ca4de7115ae3029a5eacb2
andyjefferson <andy@datanucleus.org>
2020-10-13 14:20:56 +0100

    Only run on PostgreSQL (presumably that is what the @Datastore was for but doesn't seem to work currently)

--- jdo/rdbms/src/test/org/datanucleus/tests/QueryTimeoutTest.java
+++ jdo/rdbms/src/test/org/datanucleus/tests/QueryTimeoutTest.java
@@ -28,7 +28,7 @@ import java.util.Objects;
 import static org.datanucleus.tests.annotations.Datastore.DatastoreKey.POSTGRESQL;
 
 /**
- * Test for query timeout application.
+ * Test for query timeout application, for PostgreSQL.
  */
 @Datastore({POSTGRESQL})
 public class QueryTimeoutTest extends JDOPersistenceTestCase{
@@ -70,8 +70,12 @@ public class QueryTimeoutTest extends JDOPersistenceTestCase{
         LOG.info("ended setUp");
     }
 
-    public void testJdoQLTimeout() {
-
+    public void testJdoQLTimeout() 
+    {
+        if (!vendorID.equals("postgresql"))
+        {
+            return;
+        }
         if (!pmf.supportedOptions().contains("javax.jdo.option.DatastoreTimeout"))
         {
             // Datastore doesn't support query timeouts
@@ -102,8 +106,8 @@ public class QueryTimeoutTest extends JDOPersistenceTestCase{
         // assertEquals(QueryTimeoutException.class, searcher.getThrowable().getCause().getClass());
     }
 
-    public void testSQLTimeout() {
-
+    public void testSQLTimeout() 
+    {
         /*
         DatastoreAdapter dba = getDatastoreAdapter();
         boolean lock = false;
@@ -118,6 +122,10 @@ public class QueryTimeoutTest extends JDOPersistenceTestCase{
             NucleusLogger.QUERY.warn("Requested locking of query statement, but this RDBMS doesn't support a convenient mechanism");
         }
          */
+        if (!vendorID.equals("postgresql"))
+        {
+            return;
+        }
         if (!pmf.supportedOptions().contains("javax.jdo.option.DatastoreTimeout"))
         {
             // Datastore doesn't support query timeouts
@@ -133,9 +141,12 @@ public class QueryTimeoutTest extends JDOPersistenceTestCase{
 
         t1.start();
         LOG.info(String.format("t1 started at %d", System.nanoTime()));
-        try {
+        try
+        {
             Thread.sleep(500);
-        } catch (InterruptedException e) {
+        }
+        catch (InterruptedException e)
+        {
             e.printStackTrace();
         }
         LOG.info(String.format("t2 started at %d", System.nanoTime()));
@@ -148,67 +159,87 @@ public class QueryTimeoutTest extends JDOPersistenceTestCase{
         // assertEquals(QueryTimeoutException.class, searcher.getThrowable().getCause().getClass());
     }
 
-    private void joinBoth(Thread t1, Thread t2) {
-        while (t1.isAlive() || t2.isAlive()) {
-            if (t1.isAlive()) {
-                try {
+    private void joinBoth(Thread t1, Thread t2)
+    {
+        while (t1.isAlive() || t2.isAlive())
+        {
+            if (t1.isAlive())
+            {
+                try
+                {
                     t1.join(100);
-                } catch (InterruptedException e) {
+                }
+                catch (InterruptedException e)
+                {
                     LOG.info("Still waiting for sleepy");
                 }
             }
-            if (t2.isAlive()) {
-                try {
+            if (t2.isAlive())
+            {
+                try
+                {
                     t2.join(100);
-                } catch (InterruptedException e) {
+                }
+                catch (InterruptedException e)
+                {
                     LOG.info("Still waiting for impatient");
                 }
             }
         }
     }
 
-
-    protected void tearDown() throws Exception {
+    protected void tearDown() throws Exception
+    {
         clean(Office.class);
         super.tearDown();
     }
 
-    interface Thrower {
+    interface Thrower
+    {
         Throwable getThrowable();
     }
 
-
-    interface RunnerThrowable extends Runnable, Thrower {
+    interface RunnerThrowable extends Runnable, Thrower
+    {
     }
 
     /**
      * actor that expects an answer
      */
-    static class ImpatientSearcher implements RunnerThrowable {
+    static class ImpatientSearcher implements RunnerThrowable
+    {
         final PersistenceManager pm;
+
         private final boolean sql;
+
         private final int wait;
+
         private Throwable throwable;
 
-        ImpatientSearcher(PersistenceManagerFactory pmf, boolean sql, int wait) {
+        ImpatientSearcher(PersistenceManagerFactory pmf, boolean sql, int wait)
+        {
             this.pm = pmf.getPersistenceManager();
             this.pm.setProperty(PropertyNames.PROPERTY_CACHE_L2_TYPE, "none");
             this.sql = sql;
             this.wait = wait;
         }
 
-
         @Override
-        public void run() {
+        public void run()
+        {
             LOG.debug(String.format("Running %s at %d", ImpatientSearcher.class, System.nanoTime()));
 
             final Transaction tx = pm.currentTransaction();
             tx.begin();
-            try {
+            try
+            {
                 Query q;
-                if (sql) {
+                if (sql)
+                {
                     q = pm.newQuery("javax.jdo.query.SQL", "SELECT * FROM office WHERE description = '" + DESC_PFX + "foo' FOR UPDATE ");
-                } else {
+                }
+                else
+                {
                     q = pm.newQuery("SELECT FROM " + Office.class.getName() + " WHERE description == '" + DESC_PFX + "foo'");
                     q.setSerializeRead(true);
                 }
@@ -219,11 +250,16 @@ public class QueryTimeoutTest extends JDOPersistenceTestCase{
                 LOG.info(String.format("got %s after %d", Objects.toString(r), (System.nanoTime() - t0) / 1000000));
 
                 tx.commit();
-            } catch (Throwable t) {
+            }
+            catch (Throwable t)
+            {
                 throwable = t;
                 LOG.info(String.format("got a %s", t.getClass().getSimpleName()));
-            } finally {
-                if (tx.isActive()) {
+            }
+            finally
+            {
+                if (tx.isActive())
+                {
                     tx.rollback();
                 }
                 pm.close();
@@ -231,7 +267,8 @@ public class QueryTimeoutTest extends JDOPersistenceTestCase{
         }
 
         @Override
-        public Throwable getThrowable() {
+        public Throwable getThrowable()
+        {
             return throwable;
         }
 
@@ -240,13 +277,17 @@ public class QueryTimeoutTest extends JDOPersistenceTestCase{
     /**
      * actor that locks rows and sleeps for a while
      */
-    static class SerializedSleepyLister implements Runnable {
+    static class SerializedSleepyLister implements Runnable
+    {
 
         final PersistenceManager pm;
+
         final long sleep;
+
         final boolean sql;
 
-        SerializedSleepyLister(PersistenceManagerFactory pmf, boolean sql, long sleep) {
+        SerializedSleepyLister(PersistenceManagerFactory pmf, boolean sql, long sleep)
+        {
             this.pm = pmf.getPersistenceManager();
             this.pm.setProperty(PropertyNames.PROPERTY_CACHE_L2_TYPE, "none");
             this.sleep = sleep;
@@ -254,35 +295,48 @@ public class QueryTimeoutTest extends JDOPersistenceTestCase{
         }
 
         @Override
-        public void run() {
+        public void run()
+        {
             LOG.debug(String.format("Running %s at %d", SerializedSleepyLister.class, System.nanoTime()));
             final Transaction tx = pm.currentTransaction();
             tx.begin();
-            try {
+            try
+            {
                 Query q;
-                if (sql) {
+                if (sql)
+                {
                     q = pm.newQuery("javax.jdo.query.SQL", "SELECT * FROM office FOR UPDATE");
-                } else {
+                }
+                else
+                {
                     q = pm.newQuery("SELECT FROM " + Office.class.getName());
                     q.setSerializeRead(true);
                 }
                 final List<Office> rs = (List<Office>) q.execute();
                 LOG.info(String.format("Got %d rows at %d", rs.size(), System.nanoTime()));
                 final long t0 = System.currentTimeMillis();
-                do {
+                do
+                {
                     final long t = System.currentTimeMillis();
-                    try {
+                    try
+                    {
                         final long millis = sleep - (t - t0);
                         LOG.debug(String.format("About to sleep %s", millis));
                         Thread.sleep(millis);
-                    } catch (InterruptedException e) {
+                    }
+                    catch (InterruptedException e)
+                    {
                         e.printStackTrace();
                     }
-                } while (System.currentTimeMillis() - t0 < sleep);
+                }
+                while (System.currentTimeMillis() - t0 < sleep);
                 LOG.debug(String.format("Done sleeping at %s", System.nanoTime()));
                 tx.commit();
-            } finally {
-                if (tx.isActive()) {
+            }
+            finally
+            {
+                if (tx.isActive())
+                {
                     tx.rollback();
                 }
                 pm.close();
