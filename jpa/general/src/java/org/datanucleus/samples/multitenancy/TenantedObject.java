a8acd83009320ed8719c3f7ad1f7ea1c53e38e55
andyjefferson <andy@datanucleus.org>
2021-08-26 18:09:41 +0100

    Add index

--- jpa/general/src/java/org/datanucleus/samples/multitenancy/TenantedObject.java
+++ jpa/general/src/java/org/datanucleus/samples/multitenancy/TenantedObject.java
@@ -1,12 +1,32 @@
+/**********************************************************************
+Copyright (c) 2021 Andy Jefferson and others. All rights reserved.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Contributors:
+    ...
+**********************************************************************/
 package org.datanucleus.samples.multitenancy;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.Table;
 
 import org.datanucleus.api.jpa.annotations.MultiTenant;
 
 @Entity
-@MultiTenant(columnLength = 40)
+@Table(indexes = {@Index(name = "TENANT_IDX", columnList = "TENANT_ID")})
+@MultiTenant(column = "TENANT_ID", columnLength = 40)
 public class TenantedObject
 {
     @Id
