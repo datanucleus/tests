297d1588ba96a46e261cb5d0d3ad3bde60a801bc
Kraen David Christensen <kdch@stibosystems.com>
2023-12-20 15:59:52 +0100

    Added option for supplying custom FlushProcess. Also improved support for SQL batching inserts/updates/deletes combined with optimistic lock handling.

--- jdo/rdbms/src/java/org/datanucleus/samples/models/transportation/Address.java
+++ jdo/rdbms/src/java/org/datanucleus/samples/models/transportation/Address.java
@@ -2,8 +2,11 @@ package org.datanucleus.samples.models.transportation;
 
 import javax.jdo.annotations.PersistenceCapable;
 import javax.jdo.annotations.PrimaryKey;
+import javax.jdo.annotations.Version;
+import javax.jdo.annotations.VersionStrategy;
 
 @PersistenceCapable(table = "address")
+@Version(strategy= VersionStrategy.VERSION_NUMBER, column="checkid")
 public class Address
 {
     @PrimaryKey
